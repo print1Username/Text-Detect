7d7935ee466230bb370977791532a52d9e8a5297
print1Username <128358949+print1Username@users.noreply.github.com>
2025-12-26 13:51:19 +0800

    update file location

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './components/components.css'
+import './pages/components/components.css'
 import './index.css'
-import App from './App.tsx'
+import App from './pages/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
